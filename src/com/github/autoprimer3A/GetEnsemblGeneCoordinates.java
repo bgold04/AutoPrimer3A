39498205a537a2290e9a6c92ba681d8d66c59799
bgold04 <37762452+bgold04@users.noreply.github.com>
2024-08-19 17:17:49 -0400

    provide most recent src

---
+++ src/com/github/autoprimer3A/GetEnsemblGeneCoordinates.java
@@ -0,0 +1,170 @@
+/*
+ * Copyright (C) 2014 David A. Parry <d.a.parry@leeds.ac.uk>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.github.autoprimer3A;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ *
+ * @author David A. Parry <d.a.parry@leeds.ac.uk>
+ */
+public class GetEnsemblGeneCoordinates extends GetUcscGeneCoordinates {
+    final ArrayList<String> fields = new ArrayList<>(Arrays.asList("name", "chrom", 
+            "txStart", "txEnd", "cdsStart", "cdsEnd", "exonCount", "exonStarts", 
+            "exonEnds", "strand"));  
+    @Override
+    public ArrayList<GeneDetails> getGeneFromSymbol(String symbol, String build, String db) 
+            throws SQLException, GetGeneCoordinates.GetGeneExonsException{
+        String fieldsToRetrieve = String.join(", ", fields);
+        checkConnection();
+        ArrayList<GeneDetails> transcripts = new ArrayList<>();
+        ResultSet rs2 = doQuery("SELECT COUNT(*) FROM "
+                + "information_schema.tables  WHERE table_schema = '" + build +
+                "' AND table_name = 'ensemblToGeneName';");
+        while (rs2.next()){
+            if (rs2.getInt("COUNT(*)") < 1){
+                transcripts = getTranscriptsViaKgId(symbol, build, db, fieldsToRetrieve);
+                return transcripts;
+            }else{
+                transcripts = getTranscriptsFromEnsemblToName(symbol, build, db, 
+                        fieldsToRetrieve);
+                return transcripts;
+            }
+        }
+    return transcripts;
+    }
+    
+    private ArrayList<GeneDetails> getTranscriptsFromEnsemblToName(String symbol, 
+                String build, String db, String fieldsToRetrieve) 
+            throws SQLException, GetGeneExonsException{
+        ArrayList<GeneDetails> transcripts = new ArrayList<>();
+        checkConnection();
+        System.out.println("SELECT name, value FROM '"
+            + build + ".ensemblToGeneName' WHERE value='" + symbol +"';");
+            ResultSet rs2 = doQuery("SELECT name, value FROM "
+            + build + ".ensemblToGeneName WHERE value='" + symbol +"';");
+        while (rs2.next()){
+            ResultSet rs = doQuery("SELECT " + fieldsToRetrieve + 
+                " FROM " + build + "." + db +" WHERE name='"+ rs2.getString("name") + "'");
+            transcripts.addAll(getTranscriptsFromResultSet(rs, symbol));
+        }
+        return transcripts;
+    }
+            
+    private ArrayList<GeneDetails> getTranscriptsViaKgId(String symbol, 
+            String build, String db, String fieldsToRetrieve) 
+            throws SQLException, GetGeneExonsException{
+        ArrayList<GeneDetails> transcripts = new ArrayList<>();
+        checkConnection();
+        ResultSet rs2 = doQuery("SELECT COUNT(*) FROM "
+                    + "information_schema.tables  WHERE table_schema = '"+ 
+                build +"' AND table_name = 'knownToEnsembl';");
+        while (rs2.next()){
+            if (rs2.getInt("COUNT(*)") > 0){
+                ArrayList<String> kgids = new ArrayList<>();
+                System.out.println("SELECT kgID, "
+                        + "geneSymbol FROM " + build + ".kgXref WHERE "
+                        + "geneSymbol='" + symbol +"';");
+                ResultSet rs3 = doQuery("SELECT kgID, "
+                        + "geneSymbol FROM " + build + ".kgXref WHERE "
+                        + "geneSymbol='" + symbol +"';");
+                while(rs3.next()){
+                    kgids.add(rs3.getString("kgID"));
+                }
+                System.out.println("Select value FROM " + build 
+                        + ".knownToEnsembl WHERE name='" + 
+                        String.join(" or name=", kgids) + "';");
+                ResultSet rs4 = doQuery("Select value FROM " + build 
+                        + ".knownToEnsembl WHERE name='" + 
+                        String.join(" or name=", kgids) + "';");
+                while (rs4.next()){
+                    ResultSet rs = doQuery("SELECT " + fieldsToRetrieve + 
+                        " FROM " + build + "." + db +" WHERE name='"+ rs4.getString("value") + "'");
+                    transcripts.addAll(getTranscriptsFromResultSet(rs, symbol));
+                }
+                return transcripts;
+            }else{
+                //TO DO!
+                //can't find ensemblToGene or knownToEnsembl - throw an error?
+                return  transcripts;
+            }
+        }
+        return transcripts;
+    }
+    
+    
+    @Override
+    public ArrayList<GeneDetails> getGeneFromId(String id, String build, String db) 
+            throws SQLException, GetGeneExonsException{
+        String fieldsToRetrieve = String.join(", ", fields);
+        String symbol = new String();
+        checkConnection();
+        System.out.println("SELECT " + fieldsToRetrieve + 
+                " FROM " + build + "." + db + " WHERE name='"+ id + "'");
+        ResultSet rs = doQuery("SELECT " + fieldsToRetrieve + 
+                " FROM " + build + "." + db + " WHERE name='"+ id + "'");
+        ResultSet rs2 = doQuery("SELECT COUNT(*) FROM "
+                + "information_schema.tables  WHERE table_schema = '" + build +
+                "' AND table_name = 'ensemblToGeneName';");
+        while (rs2.next()){
+            if (rs2.getInt("COUNT(*)") < 1){
+                symbol = getSymbolViaKgId(id, build);
+            }else{
+                symbol = getSymbolFromEnsemblToName(id, build);
+            }
+        }
+        return getTranscriptsFromResultSet(rs, symbol);
+    }
+    
+    private String getSymbolFromEnsemblToName(String id, String build) 
+            throws SQLException{
+        String symbol = new String();
+        checkConnection();
+        ResultSet rs2 = doQuery("SELECT name, value FROM "
+            + build + ".ensemblToGeneName WHERE name='" + id +"';");
+        while (rs2.next()){
+            symbol = rs2.getString("value");
+        }
+        return symbol;
+    }
+    
+    private String getSymbolViaKgId(String id, String build) throws SQLException{
+        String symbol = new String();
+        checkConnection();
+        ResultSet rs2 = doQuery("SELECT COUNT(*) FROM "
+                    + "information_schema.tables  WHERE table_schema = '"+ 
+                build +"' AND table_name = 'knownToEnsembl';");
+        while (rs2.next()){
+            if (rs2.getInt("COUNT(*)") > 0){
+                ResultSet rs3 = doQuery("Select name FROM " + build 
+                        + ".knownToEnsembl WHERE value='" + 
+                        id + "';");
+                while (rs3.next()){
+                    ResultSet rs4 = doQuery("SELECT geneSymbol FROM " 
+                            + build + ".kgXref WHERE kgID='" + rs3.getString("name") +"';");
+                    while (rs4.next()){
+                        symbol = rs4.getString("geneSymbol");
+                    }
+                }
+            }
+        }
+        return symbol;
+    }
+}//end of class
---
+++ src/com/github/autoprimer3A/GetGeneCoordinates.java
@@ -0,0 +1,166 @@
+/*
+ * Copyright (C) 2014 David A. Parry <d.a.parry@leeds.ac.uk>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.github.autoprimer3A;
+
+import com.mysql.jdbc.exceptions.jdbc4.CommunicationsException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+/**
+ *
+ * @author David A. Parry <d.a.parry@leeds.ac.uk>
+ */
+public class GetGeneCoordinates {//default handles RefSeq and Encode genes
+    static Connection conn;
+    final ArrayList<String> fields = new ArrayList<>(Arrays.asList("name", "chrom", 
+            "txStart", "txEnd", "cdsStart", "cdsEnd", "exonCount", "exonStarts", 
+            "exonEnds", "strand", "name2"));    
+    static {
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://genome-mysql.cse.ucsc.edu" +
+               "?user=genomep&password=password&no-auto-rehash");
+        }catch (SQLException ex){
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+    
+    public void checkConnection() throws SQLException{
+        if (conn.isClosed()){
+            conn = DriverManager.getConnection("jdbc:mysql://genome-mysql.cse.ucsc.edu" +
+               "?user=genomep&password=password&no-auto-rehash");
+        }
+    }
+    
+    
+    public ResultSet doQuery(String query) throws SQLException{
+        Statement st = conn.createStatement();
+        try{
+            return st.executeQuery(query);
+        }catch(SQLException ex){
+            //check if failed because connection has closed and if so try one more time
+            if (! (ex instanceof CommunicationsException)){
+                throw ex;
+            }
+            conn = DriverManager.getConnection("jdbc:mysql://genome-mysql.cse.ucsc.edu" +
+               "?user=genomep&password=password&no-auto-rehash");
+            st = conn.createStatement();
+            return st.executeQuery(query);
+        }
+    }
+    
+    public ArrayList<GeneDetails> getGeneFromSymbol(String symbol, String build, String db) 
+            throws SQLException, GetGeneExonsException{
+        String fieldsToRetrieve = String.join(", ", fields );
+        checkConnection();
+        ResultSet rs = doQuery("SELECT " + fieldsToRetrieve + 
+                " FROM " + build + "." + db +" WHERE name2='"+ symbol+ "'");
+        return getTranscriptsFromResultSet(rs);
+    }
+    
+    public ArrayList<GeneDetails> getGeneFromId(String id, String build, String db) 
+            throws SQLException, GetGeneExonsException{
+        String fieldsToRetrieve = String.join(", ", fields);
+        checkConnection();
+        ResultSet rs = doQuery("SELECT " + fieldsToRetrieve + 
+                " FROM " + build + "." + db + " WHERE name='"+ id + "'");
+        return getTranscriptsFromResultSet(rs);
+    }
+    
+    protected ArrayList<GeneDetails> getTranscriptsFromResultSet(ResultSet rs) 
+             throws SQLException, GetGeneExonsException{
+        ArrayList<HashMap<String, String>> genes = new ArrayList<>();
+        while (rs.next()){
+            HashMap<String, String> geneCoords = new HashMap<>();
+            for (String f: fields){
+                geneCoords.put(f, rs.getString(f));
+            }
+            genes.add(geneCoords);
+        }
+        ArrayList<GeneDetails> transcriptsToReturn = new ArrayList<>();
+        for (HashMap<String, String> gene: genes){
+            GeneDetails geneDetails = geneHashToGeneDetails(gene);
+            transcriptsToReturn.add(geneDetails);
+        }
+        return transcriptsToReturn;
+    }
+    
+    protected GeneDetails geneHashToGeneDetails(HashMap<String,String> gene) 
+            throws GetGeneExonsException{
+        GeneDetails geneDetails = new GeneDetails();
+        geneDetails.setSymbol(gene.get("name2"));
+        geneDetails.setId(gene.get("name"));
+        geneDetails.setStrand(gene.get("strand"));
+        geneDetails.setChromosome(gene.get("chrom"));
+        geneDetails.setTxStart(Integer.parseInt(gene.get("txStart")));
+        geneDetails.setTxEnd(Integer.parseInt(gene.get("txEnd")));
+        geneDetails.setCdsStart(Integer.parseInt(gene.get("cdsStart")));
+        geneDetails.setCdsEnd(Integer.parseInt(gene.get("cdsEnd")));
+        geneDetails.setTotalExons(Integer.parseInt(gene.get("exonCount")));
+        try{
+            geneDetails.setExons(gene.get("exonStarts"), gene.get("exonEnds"));
+        }catch (GeneDetails.GeneExonsException ex){
+            throw new GetGeneExonsException("Error parsing exons for transcript "
+                    + geneDetails.getId() + ", gene " + geneDetails.getSymbol(), ex);
+        }
+        return geneDetails;
+    }
+    
+    public class GetGeneExonsException extends Exception{
+        public GetGeneExonsException() { super(); }
+        public GetGeneExonsException(String message) { super(message); }
+        public GetGeneExonsException(String message, Throwable cause) { super(message, cause); }
+        public GetGeneExonsException(Throwable cause) { super(cause); }
+    }
+    
+    public ArrayList<GenomicRegionSummary> GetSnpCoordinates(
+            String chrom, int start, int end, String build, String db)
+            throws SQLException{
+        ArrayList<GenomicRegionSummary> snpCoordinates = new ArrayList<> ();
+        checkConnection();
+        //user may not have preceded chromosome with 'chr' which is fine for
+        //sequence retrieval but not for snp retrieval in genomes which use it
+        ArrayList<String> chromosomes = new ArrayList<>();
+        ResultSet chromSet = doQuery("SELECT DISTINCT(chrom) AS chrom "
+                + "FROM " + build + "." + db);
+        while (chromSet.next()){
+            chromosomes.add(chromSet.getString("chrom"));
+        }
+        if (! chromosomes.contains(chrom)){
+            chrom = "chr" + chrom;
+            if (!chromosomes.contains(chrom)){
+                return snpCoordinates;
+            }
+        }
+        ResultSet rs = doQuery("SELECT chromStart,chromEnd from " + 
+                build+ "." + db + " where chrom='" + chrom + 
+                "' and chromEND >= " + start + " and chromStart < " + end);
+        
+        while (rs.next()){
+            snpCoordinates.add(new GenomicRegionSummary(chrom,
+                    Integer.valueOf(rs.getString("chromStart")) + 1, 
+                    Integer.valueOf(rs.getString("chromEnd"))));
+        }
+        return snpCoordinates;
+    }
+
+}
